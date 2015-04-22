e3bc772b299aafc4a87e2585fcddc0a7293b092c
kumar-velugula <velugula2003@yahoo.com>
2015-04-22 00:38:27 -0400

    first commit on assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two functions created 1. to cache already created inverse and 2. to calculate inverse of matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix function creates a matrix which is a list containing a function to 
+##1. set matrix
+##2. get matrix
+##3. set inversematrix
+##4. get inversematrix
+makeCacheMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <- NULL
+        }
+        get <- function() x
+        setinversematrix <- function(inverseMatrix) m <<- inverseMatrix
+        getinversematrix <- function() m
+        list(set = set, get = get, setinversematrix = setinversematrix, getinversematrix = getinversematrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##cacheSolve function calculates inverse of a matrix using solve function as below
+##1. check if the matrix inverse is already calculated then return cached inversematrix
+##2. get source matrix data and apply solve on the matrix to return inverse
+##3. solve is wrapped in try catch to return friendly error if the matrix is not square matrix or if inverse cannot be calculated
+cacheSolve <- function(x, ...){
+        m <- x$getinversematrix()
+        if(!is.null(m)){
+                message ("returning cached data")
+                return (m)
+        }
+        data <- x$get()
+        m <- tryCatch( solve(data), error =function(e) e)
+        any(class(m) == "error")
+        m
 }
